a7baeb8e8c342ff96237185033f44625609608e0
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-04-12 18:21:21 +0200

    Added some methods. Not working but still something

--- MPP_03/src/Layer.java
+++ MPP_03/src/Layer.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Layer {
     Perceptron[] perceptrons;
@@ -8,12 +8,17 @@ public class Layer {
         this.perceptrons = perceptrons;
     }
 
-    public Layer(int size) {
+    public Layer(int size, String[] labels) {
         perceptrons = new Perceptron[size];
+        for (int i = 0; i < perceptrons.length; i++) {
+            perceptrons[i] = new Perceptron(26, labels[i]);
+        }
     }
 
     public void train(TrainingText[] trainingTexts) {
-        for (TrainingText trainingText : trainingTexts) {
+        ArrayList<TrainingText> data = new ArrayList<>(Arrays.asList(trainingTexts));
+        Collections.shuffle(data);
+        for (TrainingText trainingText : data) {
             LetterProportionVector vector = new LetterProportionVector(trainingText);
             for (Perceptron perceptron : perceptrons) {
                 int y = perceptron.compute(vector.getProportions()) >= 0 ? 1 : 0;
@@ -23,4 +28,43 @@ public class Layer {
             }
         }
     }
+
+    public String classify(TrainingText trainingText){
+        double[] rawOutput = compute(trainingText);
+        double[] normalizedOutput = maximumSelector(rawOutput);
+
+        for (int i = 0; i < perceptrons.length; i++) {
+            if(normalizedOutput[i] == 0)
+                return perceptrons[i].label;
+        }
+
+        return "FAIL";
+    }
+
+    public double[] compute(TrainingText trainingText){
+        double[] output = new double[perceptrons.length];
+        LetterProportionVector vector = new LetterProportionVector(trainingText);
+
+        for (int i = 0; i < perceptrons.length; i++) {
+            output[i] = perceptrons[i].compute(vector.getProportions());
+        }
+
+        return output;
+    }
+
+    public double[] maximumSelector(double[] input){
+        double[] output = new double[input.length];
+        double max = Arrays.stream(input).max().orElse(0);
+
+        for (int i = 0; i < input.length; i++) {
+            if(input[i] == max){
+                output[i] = 1;
+                break;
+            }
+        }
+
+        return output;
+    }
+
+
 }
--- MPP_03/src/Main.java
+++ MPP_03/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     private final static String trainingDataDir = "TrainingData";
 
     public static void main(String[] args) {
-        System.out.println("Hello World?");
+        TrainingText[] texts = TrainingText.fetchTrainingTexts(trainingDataDir);
+        int langCount = (int) Arrays.stream(texts).map(TrainingText::getLang).distinct().count();
+        String[] langs = Arrays.stream(texts).map(TrainingText::getLang).distinct().toArray(String[]::new);
+
+        Layer layer = new Layer(langCount, langs);
+        layer.train(texts);
+        System.out.println(texts[20]);
+        System.out.println(layer.classify(texts[1]));
     }
 }
