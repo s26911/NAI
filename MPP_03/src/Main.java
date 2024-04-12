c4f0babf9f2a7cb0f7a928c292135ffa591a944d
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-04-12 12:41:38 +0200

    Added TrainingText class with fetchTrainingTexts method

--- MPP_03/src/Main.java
+++ MPP_03/src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+    private final static String trainingDataDir = "TrainingData";
+
     public static void main(String[] args) {
         System.out.println("Hello World?");
     }
---
+++ MPP_03/src/TrainingText.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+
+public class TrainingText {
+    private String text;
+    private String lang;
+
+    public TrainingText(String text, String lang) {
+        this.text = filter(text);
+        this.lang = lang;
+    }
+
+    private String filter(String text) {
+        return text.replaceAll("[^a-zA-Z]", "");
+    }
+
+    public static TrainingText[] fetchTrainingTexts(String trainingDataDir){
+        ArrayList<TrainingText> trainingTexts = new ArrayList<>();
+
+        try {
+            Files.walkFileTree(Path.of(trainingDataDir), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.getFileName().toString().endsWith(".txt")) {
+                        String text = Files.readString(file);
+                        trainingTexts.add(new TrainingText(text, file.getParent().toString()));
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return trainingTexts.toArray(new TrainingText[0]);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    @Override
+    public String toString() {
+        return "LANG: " + lang + "\nTEXT: " + text.substring(0,50);
+    }
+}
