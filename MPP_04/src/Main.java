8ed4f6b88bd6912d2cf3148ff347e0f6754699e9
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-04-16 17:29:13 +0200

    Added csv file reading feature

--- MPP_04/src/Main.java
+++ MPP_04/src/Main.java
@@ -1,5 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIi");
+        ArrayList<String[]> trainSet;
+        ArrayList<String[]> testSet;
+
+        try {
+            trainSet = readCsvFile(args[0]);
+            testSet = readCsvFile(args[1]);
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found! Invalid pathname of at least one file");
+            return;
+        }
+
+        System.out.println("TRAIN SET: ");
+        trainSet.forEach(x -> System.out.println(Arrays.toString(x)));
+        System.out.println();
+
+        System.out.println("TEST SET: ");
+        testSet.forEach(x -> System.out.println(Arrays.toString(x)));
+        System.out.println();
+
+
+        //UI.start();
+    }
+
+    public static ArrayList<String[]> readCsvFile(String path) throws FileNotFoundException {
+        ArrayList<String[]> result = new ArrayList<>();
+        int numberOfColumns = 0;
+
+        try (Scanner s = new Scanner(new File(path))) {
+            for (int i = 0; s.hasNextLine(); i++) {
+                var row = s.nextLine().split(",");
+                if (i == 0) {
+                    numberOfColumns = row.length;            // set length of first row as numberOfColumns
+                    result.add(row);
+                } else if (row.length == numberOfColumns) {   // add only rows having the same size as the first one
+                    result.add(row);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw e;
+        }
+
+        return result;
     }
 }
