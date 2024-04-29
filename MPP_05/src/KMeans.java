f62605bb4a597171666e06f3d0e3f5ead6562269
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-04-29 21:16:02 +0200

    Added assignGroups() and calculateDistance(), changed initCentroids()

--- MPP_05/src/KMeans.java
+++ MPP_05/src/KMeans.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class KMeans {
     ArrayList<double[]> data;
@@ -20,22 +21,52 @@ public class KMeans {
         // 4 calculate centroids
         // 5 back to 2
 
-        // ...
+        ArrayList<double[]> centroids = initCentroids();
+        while (true) {
+            HashMap<Integer, ArrayList<double[]>> groups = assignGroups(centroids);
+            // ...
+        }
     }
 
     private ArrayList<double[]> initCentroids() {
         ArrayList<double[]> centroids = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            double[] centroid = new double[rowSize];
-            for (int j = 0; j < rowSize; j++) {
-                centroid[j] = Math.random() * 5;
-            }
-            centroids.add(centroid);
+        ArrayList<double[]> dataCopy = new ArrayList<>(data);
+        while(centroids.size() < k) {
+            int i = (int) (Math.random() * dataCopy.size());
+            centroids.add(dataCopy.get(i));
+            dataCopy.remove(i);
         }
         return centroids;
     }
-    public HashMap<Integer, ArrayList<double[]>> assignGroups() {
-        // ...
+
+    public HashMap<Integer, ArrayList<double[]>> assignGroups(ArrayList<double[]> centroids) {
+        HashMap<Integer, ArrayList<double[]>> groups = new HashMap<>();
+
+        for (double[] row : data) {
+            int centroidIndex = 0;
+            double distance = calculateDistance(row, centroids.get(0));
+
+            for (int i = 1; i < k; i++) {
+                double currDistance = calculateDistance(row, centroids.get(i));
+                if (currDistance < distance) {
+                    distance = currDistance;
+                    centroidIndex = i;
+                }
+            }
+
+            groups.putIfAbsent(centroidIndex, new ArrayList<>());
+            groups.get(centroidIndex).add(row);
+        }
+
+        return groups;
+    }
+
+    public double calculateDistance(double[] v1, double[] v2) {
+        double sum = 0;
+        for (int i = 0; i < v1.length; i++) {
+            sum += Math.pow(v1[i] - v2[i], 2);
+        }
+        return Math.sqrt(sum);
     }
 
     public double calculateE() {
