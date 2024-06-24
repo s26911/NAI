b6763c2cbe003e1bd896e4ee6ae054a4465c2128
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-06-24 12:26:07 +0200

    update: it doesn't work.

--- MPP_08/src/KnapsackSA.java
+++ MPP_08/src/KnapsackSA.java
@@ -6,7 +6,8 @@ public class KnapsackSA {
     int[] weights, values;
 
     // config
-    int maxStagnationCount = 100, minTemp = 0, maxIterations = 1000;
+    int maxStagnationCount = 10000, minTemp = 0, maxIterations = 10000;
+    double limitExceededMultiplier = 4, probabilityMultiplier = 0.2;
 
     public KnapsackSA(ArrayList<int[]> data) {
         int[] firstRow = data.get(0);
@@ -20,58 +21,112 @@ public class KnapsackSA {
         maxValue = Arrays.stream(values).sum();
     }
 
-    public int solve(){
-        // 1. start from random point (solution) X
-        // 2. check end condition (end or continue)
-        // 3. select random neighbour of X
-        // 4. if neighbour:
-            // a. has higher value: set as new X
-            // b. has lower or equal value:
-                // b1. calculate probability P [0,1]
-                // b2. compare P with random number [0,1] and set neighbour as X or continue
-        // 5. lower temp
-        // 6. go to 2
+    public int solve() {
+        int curr = (int) (Math.random() * Math.pow(2, numberOfItems));   // random starting solution
+        int next = 0, iterCounter = 0, stagnationCounter = 0;
+        double temp = initTemp;
 
-        // random starting solution
-        int curr = (int) (Math.random() * Math.pow(2,numberOfItems));
-        int next = 0;
-        int iterCounter = 0;
-        while(!stopCondition(iterCounter++)){
+        while (!stopCondition(iterCounter++, stagnationCounter)) {
+            boolean stagnated = true;
             next = randomNeighbour(curr);
-            //TODO
+            boolean o1 = overTheLimit(curr);
+            boolean o2 = overTheLimit(next);
+            double v1 = calculateValue(curr);
+            double v2 = calculateValue(next);
+
+            if (compareSolutions(curr, next) < 0) {
+                curr = next;
+                stagnated = false;
+            } else {
+                double P = calculateProbability(curr, next, temp);
+                double r = Math.random();
+                if (r < P) {
+                    curr = next;
+                    stagnated = false;
+                }
+            }
+
+            temp = decreaseTemp(temp);
+            if (stagnated)
+                stagnationCounter++;
+            else
+                stagnationCounter = 0;
         }
 
-        return 1;
+        return curr;
+    }
+
+    private double decreaseTemp(double temp) {
+        if (tempDelta != 0)
+            return temp - tempDelta;
+        else
+            return temp - 1;
     }
 
     // returns true if algorithm should be stopped
-    private boolean stopCondition(int iteration){
-        return iteration > maxStagnationCount;
+    private boolean stopCondition(int iteration, int stagnationCounter) {
+        return iteration > maxIterations || stagnationCounter > maxStagnationCount;
     }
 
-    private int randomNeighbour(int current){
+    private int randomNeighbour(int current) {
         int bitToChange = (int) (Math.random() * numberOfItems);
-        if ((current & (1<<bitToChange)) != 0)      // if current has 1 on "bitToChange" index
-            return current & ~(1<<bitToChange);     // then change it to 0
+        if ((current & (1 << bitToChange)) != 0)      // if current has 1 on "bitToChange" index
+            return current & ~(1 << bitToChange);     // then change it to 0
         else
-            return current | (1<<bitToChange);      // else change it to 1
+            return current | (1 << bitToChange);      // else change it to 1
     }
 
-    // funkcja celu
-    private double calculateScore(int solution){
-        // 0-0.5 over the limit     + lesser value has lesser score if both are over the limit
-        // 0.5-1 less than the limit    + the solution compelling with the space limitation is always better than exceeding the limit
+    private int compareSolutions(int s1, int s2) {
+        if (overTheLimit(s1) && !overTheLimit(s2))
+            return -1;
+        if (!overTheLimit(s1) && overTheLimit(s2))
+            return 1;
 
-        //TODO
-        return 1.;
+        double s1v = calculateValue(s1), s2v = calculateValue(s2);
+        if (s1v == s2v)
+            return 0;
+        else
+            return (s1v < s2v) ? -1 : 1;
     }
 
-    private boolean overTheLimit(int solution){
+    public boolean overTheLimit(int solution) {
         int size = 0;
         for (int i = 0; i < numberOfItems; i++) {
             if ((solution & (1 << i)) != 0)
                 size += weights[i];
         }
         return size >= capacity;
+    }
+
+    public double calculateValue(int solution) {
+        int value = 0;
+        for (int i = 0; i < numberOfItems; i++) {
+            if ((solution & (1 << i)) != 0)
+                value += values[i];
+        }
+
+        if (overTheLimit(solution)){
+            double capacityOverhead = getSolutionSize(solution) - capacity;
+            return value - (capacityOverhead * limitExceededMultiplier);
+        }
+        return value;
+    }
+    public int getSolutionSize(int solution) {
+        int size = 0;
+        for (int i = 0; i < numberOfItems; i++) {
+            if ((solution & (1 << i)) != 0)
+                size += weights[i];
+        }
+        return size;
+    }
+
+    private double calculateProbability(int curr, int next, double temp) {
+//        double P = Math.exp(-((Math.abs(calculateValue(curr) - calculateValue(next))) / (temp)));
+        double v1 = calculateValue(curr);
+        double v2 = calculateValue(next);
+        double mabs = -(Math.abs(v1 - v2));
+        double P = Math.exp(mabs/temp) * probabilityMultiplier;
+
+        return P;
     }
 }
--- MPP_08/src/Main.java
+++ MPP_08/src/Main.java
@@ -8,7 +8,17 @@ public class Main {
             System.out.println(Arrays.toString(line));
         }
 
+        double best = 0;
         KnapsackSA knapsack = new KnapsackSA(data);
-        knapsack.solve();
+        for (int i = 0; i < 100; i++) {
+            int sol = knapsack.solve();
+            int size = 0;
+            double val = knapsack.calculateValue(sol);
+            if (val > best)
+                best = val;
+            System.out.println(Util.NBytesAsString(sol, data.get(0)[1]) + "\t" + knapsack.overTheLimit(sol) +"\t" + knapsack.getSolutionSize(sol) + "\t" + val);
+        }
+        System.out.println("\n\nBest: " + best);
+
     }
 }
